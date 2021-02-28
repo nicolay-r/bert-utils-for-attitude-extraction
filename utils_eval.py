f35ba777a58418ece356abf96e00d097e7a9ca3b
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-02-28 16:54:00 +0300

    #2, parameters were removed.

--- utils_eval.py
+++ utils_eval.py
@@ -1,23 +1,21 @@
 import argparse
 
+from arekit.common.entities.formatters.types import EntityFormattersService
 from arekit.common.evaluation.evaluators.modes import EvaluationModes
 from arekit.common.evaluation.evaluators.two_class import TwoClassEvaluator
 from arekit.common.experiment.data_type import DataType
 from arekit.common.experiment.folding.types import FoldingType
 from arekit.common.experiment.scales.factory import create_labels_scaler
+
 from arekit.contrib.bert.output.eval_helper import EvalHelper
 from arekit.contrib.bert.run_evaluation import LanguageModelExperimentEvaluator
+from arekit.contrib.bert.samplers.types import SampleFormattersService, BertSampleFormatterTypes
 from arekit.contrib.experiments.factory import create_experiment
+from arekit.contrib.experiments.types import ExperimentTypes
+from arekit.contrib.source.ruattitudes.io_utils import RuAttitudesVersionsService
+from arekit.contrib.source.rusentiframes.types import RuSentiFramesVersionsService
 from arekit.contrib.source.rusentrel.opinions.formatter import RuSentRelOpinionCollectionFormatter
-from args.balance import UseBalancingArg
-from args.bert_formatter import BertInputFormatterArg
-from args.cv_index import CvCountArg
-from args.dist_in_terms_between_ends import DistanceInTermsBetweenAttitudeEndsArg
-from args.entity_fmt import EnitityFormatterTypesArg
-from args.experiment import ExperimentTypeArg
-from args.frames import RuSentiFramesVersionArg
-from args.labels_count import LabelsCountArg
-from args.ra_ver import RuAttitudesVersionArg
+
 from args.rusentrel import RuSentRelVersionArg
 from args.stemmer import StemmerArg
 from args.terms_per_context import TermsPerContextArg
@@ -48,77 +46,91 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='*.tsv results based evaluator')
 
-    ExperimentTypeArg.add_argument(parser)
-    CvCountArg.add_argument(parser)
-    RuSentRelVersionArg.add_argument(parser)
-    LabelsCountArg.add_argument(parser)
-    RuAttitudesVersionArg.add_argument(parser)
-    UseBalancingArg.add_argument(parser)
-    TermsPerContextArg.add_argument(parser)
-    DistanceInTermsBetweenAttitudeEndsArg.add_argument(parser)
-    StemmerArg.add_argument(parser)
-    RuSentiFramesVersionArg.add_argument(parser)
-    BertInputFormatterArg.add_argument(parser)
-    EnitityFormatterTypesArg.add_argument(parser)
+    parser.add_argument('--max-epochs',
+                        dest="max_epochs",
+                        type=int,
+                        default=200,
+                        nargs='?',
+                        help="Labels count in an output classifier")
 
     # Parsing arguments.
     args = parser.parse_args()
 
-    stemmer = StemmerArg.read_argument(args)
-    labels_count = LabelsCountArg.read_argument(args)
-    exp_type = ExperimentTypeArg.read_argument(args)
-    cv_count = CvCountArg.read_argument(args)
-    rusentrel_version = RuSentRelVersionArg.read_argument(args)
-    sample_formatter_type = BertInputFormatterArg.read_argument(args)
-    entity_formatter_type = EnitityFormatterTypesArg.read_argument(args)
-    labels_scaler = create_labels_scaler(labels_count)
-    ra_version = RuAttitudesVersionArg.read_argument(args)
-    folding_type = FoldingType.Fixed if cv_count == 1 else FoldingType.CrossValidation
-    balance_samples = UseBalancingArg.read_argument(args)
-    terms_per_context = TermsPerContextArg.read_argument(args)
-    dist_in_terms_between_attitude_ends = DistanceInTermsBetweenAttitudeEndsArg.read_argument(args)
-    frames_version = RuSentiFramesVersionArg.read_argument(args)
-    experiment_io_type = CustomBertIOUtils
-    eval_mode = EvaluationModes.Extraction if labels_count == 3 else EvaluationModes.Classification
+    # Constant predefined parameters.
+    max_epochs_count = args.max_epochs
+    rusentrel_version = RuSentRelVersionArg.default
+    terms_per_context = TermsPerContextArg.default
+    stemmer = StemmerArg.supported[StemmerArg.default]
+    eval_mode = EvaluationModes.Extraction
+    dist_in_terms_between_attitude_ends = None
 
-    full_model_name = Common.create_full_model_name(sample_fmt_type=sample_formatter_type,
-                                                    entities_fmt_type=entity_formatter_type,
-                                                    labels_count=int(labels_count))
+    # grid for looking through.
+    grid = {
+        u"labels": [2, 3],
+        u"foldings": [FoldingType.Fixed,
+                      FoldingType.CrossValidation],
+        u"exp_types": [ExperimentTypes.RuSentRel,
+                       ExperimentTypes.RuSentRelWithRuAttitudes],
+        u"entity_fmts": [EntityFormattersService.get_type_by_name(ent_fmt)
+                         for ent_fmt in EntityFormattersService.iter_supported_names()],
+        u"sample_types": [fmt_type for fmt_type in BertSampleFormatterTypes],
+        u"ra_names": [RuAttitudesVersionsService.find_by_name(ra_name)
+                      for ra_name in RuAttitudesVersionsService.iter_supported_names()],
+        u'balancing': [True],
+        u"frames_versions": [RuSentiFramesVersionsService.get_type_by_name(frames_version)
+                             for frames_version in RuSentiFramesVersionsService.iter_supported_names()],
+    }
 
-    extra_name_suffix = create_exp_name_suffix(use_balancing=balance_samples,
-                                               terms_per_context=terms_per_context,
-                                               dist_in_terms_between_att_ends=dist_in_terms_between_attitude_ends)
+    def __run():
 
-    model_io = BertModelIO(full_model_name=full_model_name)
+        full_model_name = Common.create_full_model_name(sample_fmt_type=sample_formatter_type,
+                                                        entities_fmt_type=entity_formatter_type,
+                                                        labels_count=int(labels_count))
 
-    # Setup default evaluator.
-    evaluator = TwoClassEvaluator(eval_mode)
+        extra_name_suffix = create_exp_name_suffix(use_balancing=balance_samples,
+                                                   terms_per_context=terms_per_context,
+                                                   dist_in_terms_between_att_ends=dist_in_terms_between_attitude_ends)
 
-    experiment_data = CustomTrainingData(
-        labels_scaler=create_labels_scaler(labels_count),
-        stemmer=stemmer,
-        evaluator=evaluator,
-        opinion_formatter=RuSentRelOpinionCollectionFormatter(),
-        model_io=model_io,
-        callback=CustomCallback(DataType.Test))
+        model_io = BertModelIO(full_model_name=full_model_name)
 
-    # Composing experiment.
-    experiment = create_experiment(exp_type=exp_type,
-                                   experiment_data=experiment_data,
-                                   folding_type=FoldingType.Fixed if cv_count == 1 else FoldingType.CrossValidation,
-                                   rusentrel_version=rusentrel_version,
-                                   experiment_io_type=CustomBertIOUtils,
-                                   ruattitudes_version=ra_version,
-                                   load_ruattitude_docs=False,
-                                   extra_name_suffix=extra_name_suffix)
+        # Setup default evaluator.
+        evaluator = TwoClassEvaluator(eval_mode)
 
-    eval_helper = CustomEvalHelper(log_dir=Common.log_dir,
-                                   state_name=u"multi_cased_L-12_H-768_A-12")
+        experiment_data = CustomTrainingData(
+            labels_scaler=create_labels_scaler(labels_count),
+            stemmer=stemmer,
+            evaluator=evaluator,
+            opinion_formatter=RuSentRelOpinionCollectionFormatter(),
+            model_io=model_io,
+            callback=CustomCallback(DataType.Test))
 
-    engine = LanguageModelExperimentEvaluator(experiment=experiment,
-                                              data_type=DataType.Test,
-                                              eval_helper=eval_helper,
-                                              max_epochs_count=100)
+        # Composing experiment.
+        experiment = create_experiment(exp_type=exp_type,
+                                       experiment_data=experiment_data,
+                                       folding_type=folding_type,
+                                       rusentrel_version=rusentrel_version,
+                                       experiment_io_type=CustomBertIOUtils,
+                                       ruattitudes_version=ra_version,
+                                       load_ruattitude_docs=False,
+                                       extra_name_suffix=extra_name_suffix)
 
-    # Starting evaluation process.
-    engine.run()
+        eval_helper = CustomEvalHelper(log_dir=Common.log_dir,
+                                       state_name=u"multi_cased_L-12_H-768_A-12")
+
+        engine = LanguageModelExperimentEvaluator(experiment=experiment,
+                                                  data_type=DataType.Test,
+                                                  eval_helper=eval_helper,
+                                                  max_epochs_count=max_epochs_count)
+
+        # Starting evaluation process.
+        engine.run()
+
+    for labels_count in grid[u"labels"]:
+        for folding_type in grid[u"foldings"]:
+            for exp_type in grid[u'exp_types']:
+                for entity_formatter_type in grid[u'entity_fmts']:
+                    for sample_formatter_type in grid[u'sample_types']:
+                        for balance_samples in grid[u'balancing']:
+                            for ra_version in grid[u'ra_names']:
+                                for frames_version in grid[u'frames_versions']:
+                                    __run()
